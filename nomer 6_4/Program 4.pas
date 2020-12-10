c1f189b0e9a54926642ce3784ab163184a5b34c2
alexandr <alex.danile@mail.ru>
2020-12-10 15:36:45 +0200

    LucruInd

---
+++ nomer 6_4/Program 4.pas
@@ -0,0 +1,14 @@
+﻿program nomer2;
+var n,i,rest:integer;
+begin 
+  
+  for i:=10 to 99 do 
+    begin
+    rest:= i mod 10;
+    if rest<>0 then
+      if rest mod 3 = 0
+      then 
+      
+      writeln('numere divizibile ce 3: ', i, ' ');
+end;
+end.
---
+++ nomer 6_4/Programa 1.pas
@@ -0,0 +1,10 @@
+﻿Program niomer4;
+var n,i,s:integer;
+begin
+  write('introduceti lungimea sirului = ');
+  read(n);
+  s:=0;
+  for i:=1 to n do
+    if (i mod 2 = 0) then s:= s + i;
+   writeln('suma numerele pare este = ', s);
+end.
